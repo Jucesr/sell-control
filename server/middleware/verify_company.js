1c3aff5abf29dfd1977b709b5dd94cae9b2715e4
Julio Ojeda <jcom.94m@gmail.com>
2018-07-05 10:21:25 -0700

    Verify is Company is valid in database

--- server/middleware/verify_company.js
+++ server/middleware/verify_company.js
@@ -1,12 +1,31 @@
+var {User} = require('../models/user');
+var {Company} = require('../models/company');
+
 export const verify_company = (req, res, next) => {
   const user = req.user;
 
-  if(!user.company_id){
+  if(!user.selected_company_id){
       res.status(401).send({
         error: 'User does not belong to a company'
       });
   }else{
-    req.body.company_id = user.company_id
-    next();
+    Company.findById(user.selected_company_id).then(
+      doc => {
+        if(!doc){
+          res.status(404).send({
+            error: 'Company does not exists'
+          });
+        }
+        let result = doc.users.find(u => u.equals(user._id))
+        if(result){
+          req.body.company_id = user.selected_company_id
+          next();
+        }else{
+          res.status(401).send({
+            error: 'User does not belong to a company'
+          });
+        }
+      }
+    );
   }
 };
